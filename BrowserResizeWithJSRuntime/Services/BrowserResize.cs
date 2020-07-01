03ab15b05251e96c6dac8d7ddea7664107d95ce1
Will Boxx <wboxx1@gmail.com>
2020-07-01 16:05:55 -0500

    my updates to the fork

---
+++ BrowserResizeWithJSRuntime/Services/BrowserResize.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BrowserResizeWithJSRuntime.Services
+{
+    public class BrowserResize
+    {
+        public static event Func<Task> OnResize;
+
+        [JSInvokable]
+        public static async Task OnBrowserResize()
+        {
+            await OnResize?.Invoke();
+        }
+
+        public static async Task<int> GetInnerHeight(IJSRuntime jsRuntime)
+        {
+            return await jsRuntime.InvokeAsync<int>("browserResize.getInnerHeight");
+        }
+
+        public static async Task<int> GetInnerWidth(IJSRuntime jsRuntime)
+        {
+            return await jsRuntime.InvokeAsync<int>("browserResize.getInnerWidth");
+        }
+
+    }
+}
---
+++ BrowserResizeWithJSRuntime/wwwroot/js/browser-resize.js
@@ -0,0 +1,20 @@
+﻿var globalResizeTimer = null;
+
+window.browserResize = {
+    getInnerHeight: function () {
+        return window.innerHeight;
+    },
+    getInnerWidth: function () {
+        return window.innerWidth;
+    },
+    registerResizeCallback: function () {
+        window.addEventListener("resize", browserResize.resized);
+    },
+    resized: function () {
+        //DotNet.invokeMethod("BrowserResize", 'OnBrowserResize');
+        if (globalResizeTimer != null) window.clearTimeout(globalResizeTimer);
+        globalResizeTimer = window.setTimeout(function () {
+            DotNet.invokeMethodAsync("BrowserResizeWithJSRuntime", "OnBrowserResize").then(data => data);
+        }, 200);
+    }
+}
